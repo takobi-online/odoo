33cca1cc669600f3a91c476174bcec1a139efb25
eLBati <elbaddy@gmail.com>
2019-03-27 21:44:15 +0100

    versioning

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ lib_name = 'odoo'
 
 setup(
     name='odoo',
-    version='12.0.post20190305',
+    version='12.0.post20190327',
     description=description,
     long_description=long_desc,
     url=url,
